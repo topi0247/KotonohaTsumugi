2152e1d3d58bc2edd446345aa6a6a64873447580
Kanae-Nishina <karubona.ra247@gmail.com>
2024-03-20 02:18:21 +0900

    CORS対策のURL修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8000", "https://relay-writer.vercel.app"
+    origins "http://localhost:8000", "https://kotonoha-tsumugi.vercel.app"
 
     resource "*",
       headers: :any,
