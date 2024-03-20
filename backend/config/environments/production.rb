544e900c309c654c90c2e250b6d2887af2b3d659
Kanae-Nishina <karubona.ra247@gmail.com>
2024-03-21 02:06:22 +0900

    異なるドメインでもCookie送受信を許可の設定追加

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -87,5 +87,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.session_store :cookie_store, key: "_kotonoha_tsumugi_session"
+
+  # 異なるドメイン間でのCookieの送受信を許可する
+  config.session_store :cookie_store, key: "_kotonoha_tsumugi_session", domain: "kotonoha-tsumugi.vercel.app", secure: true, same_site: :none
 end
